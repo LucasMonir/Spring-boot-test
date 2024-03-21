072aff978d20c8795771a678370ef3c32d2d5717
LucasMonir <lucasmonir@protonmail.com>
2024-03-21 11:15:58 -0300

    First commit, project and base entity created

---
+++ src/main/java/com/lucas/musicTest/MusicTestApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.musicTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicTestApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/lucas/musicTest/models/MusicModel.java
@@ -0,0 +1,41 @@
+package com.lucas.musicTest.models;
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Table(name = "song")
+public class MusicModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    private String name;
+    private int length;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int legth) {
+        this.length = legth;
+    }
+}
